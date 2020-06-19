d5cfc6e0bec629e5f4fee6439af7da9a80ab65ae
sanjaymajhi <1sanjaymajhi@gmail.com>
2020-06-19 14:12:38 +0530

    added models

---
+++ models/author.js
@@ -0,0 +1,8 @@
+const Mongoose = require("mongoose");
+const Schema = Mongoose.Schema;
+
+const author = new Schema({
+  pseudonym: { type: "String", required: true },
+});
+
+module.exports = Mongoose.model("book_sell_auhtor", author);
---
+++ models/book.js
@@ -0,0 +1,12 @@
+const Mongoose = require("mongoose");
+const Schema = Mongoose.Schema;
+
+const author = new Schema({
+  title: { type: String, required: true },
+  author: { type: Schema.Types.ObjectId, ref: "book_sell_auhtor" },
+  description: { type: String, required: true },
+  cover_image: { type: String, default: null }, // will used to store links to image by using cloudinary but not implementing it
+  price: { type: Number, required: true },
+});
+
+module.exports = Mongoose.model("book_sell_auhtor", author);
